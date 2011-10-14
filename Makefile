62f53f38fc831768d4d17d113880d3781e602c20
Andreas Zwinkau <beza1e1@web.de>
2011-10-14 22:37:36 +0200

    Add libclutter

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CFLAGS=-Wall -pedantic -g -std=c99
 LDFLAGS=-lm
 
-LIBS=cairo poppler-glib
+LIBS=cairo poppler-glib clutter-1.0
 CFLAGS+=$(shell pkg-config --cflags $(LIBS))
 LDFLAGS+=$(shell pkg-config --libs $(LIBS))
 
--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <cairo.h>
 #include <poppler.h>
+#include <clutter/clutter.h>
 #include <math.h>
 #include <assert.h>
 
@@ -16,6 +17,27 @@ int main(int argc, char** argv)
    cairo_surface_t *surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, (int)ceil(doc_w), (int)ceil(doc_h));
    cairo_t *cr = cairo_create(surface);
    poppler_page_render(page, cr);
+
+   if (clutter_init (&argc, &argv) != CLUTTER_INIT_SUCCESS)
+       return 1;
+
+   ClutterActor    *stage = clutter_stage_get_default ();
+   ClutterColor     stage_color = { 0x0, 0x0, 0x0, 0xff };
+   clutter_stage_set_color (CLUTTER_STAGE (stage), &stage_color);
+
+   /* Create a timeline to manage animation */
+   ClutterTimeline *timeline = clutter_timeline_new (6000);
+   clutter_timeline_set_loop (timeline, TRUE);
+
+   clutter_actor_show (stage);
+
+   clutter_timeline_start (timeline);
+
+   g_signal_connect (stage, "key-press-event",
+         G_CALLBACK (clutter_main_quit),
+         NULL);
+
+   clutter_main();
 
    cairo_surface_destroy(surface);
    return 0;
